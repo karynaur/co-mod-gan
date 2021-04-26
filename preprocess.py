7c7fd2938bd23e49467925bf92d1aa6029000110
Aditya Srinivas Menon <adityasrinivas.menon@gmail.com>
2021-04-26 21:15:56 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,16 @@ image=glob.glob('data/validation/*')
 val_no=len(image)
 image.extend(glob.glob('data/training/*'))
 train_no=len(image)-val_no
+
+failed=0
 for i in image:
   im1=cv2.imread(i)
-  if im1.shape!=(512,512,3):
-    resize=cv2.resize(im1,(512,512))
-    cv2.imwrite(i,resize)
-
-print(f"Resizing Sccessful! You have {train_no} training images and {val_no} validation images.")
+  try:
+    if im1.shape!=(512,512,3):
+      resize=cv2.resize(im1,(512,512))
+      cv2.imwrite(i,resize)
+  except AttributeError:
+    failed+=1
+    continue
+print(f"Resizing Sccessful! You have {train_no-failed} training images and {val_no} validation images.")
 
